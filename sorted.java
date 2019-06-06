b77df5b5bd8505ad5c4c67311e6264f979418a81
lavanyachitra <lavanya19399@gmail.com>
2019-06-06 16:15:55 +0530

    Create sorted.java

---
+++ sorted.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class sorted
+{
+static void sort(int[]ar,int n)
+{
+for(int i=0;i<n;i++)
+{
+ar[i]=i+1;
+}
+}
+public static void main(String args[])
+{
+int []ar={10,7,9,2,8,3,5,4,6,1}
+int n=ar.length;
+sot(ar,n);
+for(int i=0;i<n;i++)
+Sytem.out.println(ar[i]+" ");
+}
+}
+
+
