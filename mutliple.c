d3eb86539295846068d5a13e96cde8899a5e6041
lavanyachitra <lavanya19399@gmail.com>
2019-06-06 14:34:44 +0530

    Create mutliple.c

---
+++ mutliple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ longint sum=0,i,n,ld;
+ clrscr();
+ printf("\n given the value of n:");
+ scanf("%ld",&n);
+ for(i=1;i<=n;i++)
+ {
+ printf("\n 5 * %id = %d",i,5*i);
+ }
+ getch();
+} 
